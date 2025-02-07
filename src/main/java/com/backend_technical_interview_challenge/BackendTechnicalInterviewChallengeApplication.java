3f84c0f652d4977595729db02c9b3e3b717b92e3
Gabriel Troni <troni.gabriel.10@gmail.com>
2025-02-06 21:48:18 -0300

    feat: initialize project

---
+++ src/main/java/com/backend_technical_interview_challenge/BackendTechnicalInterviewChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.backend_technical_interview_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTechnicalInterviewChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTechnicalInterviewChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend_technical_interview_challenge/BackendTechnicalInterviewChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend_technical_interview_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTechnicalInterviewChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
