6c6b7e936ff511a66dd508251d5c134d87fd6119
Gabriel Troni <troni.gabriel.10@gmail.com>
2025-02-07 21:53:57 -0300

    feat: create transaction repository

---
+++ src/main/java/com/backend_technical_interview_challenge/repository/TransactionRepository.java
@@ -0,0 +1,8 @@
+package com.backend_technical_interview_challenge.repository;
+
+import com.backend_technical_interview_challenge.domain.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    //public Transaction createTransaction(Transaction transaction);
+}
