a4b65450661ff900ecbdf1cbb2f5a5338f3c889a
Gabriel Troni <troni.gabriel.10@gmail.com>
2025-02-07 21:53:36 -0300

    feat: create user repository

---
+++ src/main/java/com/backend_technical_interview_challenge/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.backend_technical_interview_challenge.repository;
+
+import com.backend_technical_interview_challenge.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    public Optional<User> findUserByDocument(String document);
+
+    public Optional<User> findUserById(Long id);
+}
